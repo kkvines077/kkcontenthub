0c312195a419c57de485996f817bf8eda68ac0a5
kkvines077 <74455691+kkvines077@users.noreply.github.com>
2025-07-07 07:42:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -37,3 +37,9 @@ window.onload = () => {
 function addToCart(id) {
   alert("Product " + id + " added to cart!");
 }
+function addToCart(id) {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  cart.push(id);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert("Product added to cart!");
+}
