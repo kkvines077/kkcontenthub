507fd5ce06f33cfa0fd498ae7aa00fea0fc630d8
kkvines077 <74455691+kkvines077@users.noreply.github.com>
2025-07-20 20:16:56 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,18 +1,17 @@
 function generateSuggestions(text) {
   const emojiMap = {
-    "😊": ["You look happy!", "Glad to hear that!", "Spread the joy!"],
-    "😢": ["Oh no! I'm here for you.", "It’ll get better!"],
-    "😂": ["Haha! Good one!", "That’s hilarious!"],
-    "❤️": ["Sending love!", "That’s so sweet!"],
-    "👍": ["Got it!", "Awesome!", "Nice!"]
+    "😊": ["You look happy!", "Glad to hear that!", "Stay cheerful!"],
+    "😢": ["I'm here for you.", "Don't be sad!", "Things will get better."],
+    "❤️": ["Love you too!", "Sending hugs!", "So sweet!"],
+    "😂": ["Haha!", "That was funny!", "LOL!"],
+    "👍": ["Got it!", "Sounds good!", "Okay!"]
   };
 
   if (emojiMap[text]) return emojiMap[text];
 
   const input = text.toLowerCase();
   if (input.includes("hello")) return ["Hi!", "Hello there!", "How are you?"];
-  if (input.includes("how are you")) return ["I'm great! You?", "Doing well, thanks!"];
-  if (input.includes("bye")) return ["Bye!", "See you soon!", "Take care!"];
-
-  return ["Interesting!", "Tell me more!", "Really?"];
+  if (input.includes("how are you")) return ["I'm good! You?", "Doing great, thanks!"];
+  if (input.includes("bye")) return ["Bye!", "Take care!", "See you soon!"];
+  return ["Interesting!", "Really?", "Tell me more!"];
 }
