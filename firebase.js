908e11c75249a1b047686e12eb765bc954c85d82
kkvines077 <74455691+kkvines077@users.noreply.github.com>
2025-07-20 20:47:00 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,13 @@
 // firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCbUpVvgXUF_-Y2cgs8YuKY5QVEP9apntI",
   authDomain: "smartchatapp-8a0b9.firebaseapp.com",
   projectId: "smartchatapp-8a0b9",
-  storageBucket: "smartchatapp-8a0b9.firebasestorage.app",
+  storageBucket: "smartchatapp-8a0b9.appspot.com",
   messagingSenderId: "1069569988553",
   appId: "1:1069569988553:web:ed27462ba6d8527dcc3a18",
   measurementId: "G-SRC407E9SD"
